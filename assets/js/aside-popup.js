6ead6fd93d4365d4b08f6d7e6f6fafafc16ca5b9
alumcolomina <lhintermulleescobedo@alumnat.iessacolomina.es>
2026-01-26 02:04:11 +0100

    arreglar error de popup

--- assets/js/aside-popup.js
+++ assets/js/aside-popup.js
@@ -1,36 +1,5 @@
-/*
+
   // Codigo basico
-  function closeAd() {
-    document.getElementById("adPopup").style.display = "none";
-  }
-
-*/
-/*
-  const popup = document.getElementById("adPopup");
-
-  function showAd() {
-    popup.style.display = "block";
-  }
-
-  function closeAd() {
-    popup.style.display = "none";
-  }
-
-  // Mostrar el popup cada 30 segundos
-  setInterval(showAd, 30000);
-*/
-
-/*
-// Codigo basico 
-function closeAd() {
-  document.getElementById("adPopup").style.display = "none";
-}
-if (document.getElementById("adPopup")) 
-  setTimeout(function() {
-    document.getElementById("adPopup").style.display = "block";
-  }, 30000); // Mostrar el popup despues de 30 segundos
-
-*/
   function closeAd() {
     document.getElementById("adPopup").style.display = "none";
   }
